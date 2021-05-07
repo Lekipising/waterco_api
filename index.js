89b7f5ef64a7cd8ed748e862c2e98b6bd6adbcd6
lekipising <leeplanke@gmail.com>
2021-05-07 13:49:05 +0200

    deploy

--- index.js
+++ index.js
@@ -24,28 +24,28 @@ app.listen(port, ()=>{
 });
 
 
-// Add headers
-app.use(function (req, res, next) {
+// // Add headers
+// app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://waterrw.herokuapp.com/');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', 'origin', 'Authorization');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', 'origin', 'Authorization');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    if ('OPTIONS' == req.method) {
-      res.sendStatus(200);
-    }
-    else {
-      next();
-    }});
+//     // Pass to next layer of middleware
+//     if ('OPTIONS' == req.method) {
+//       res.sendStatus(200);
+//     }
+//     else {
+//       next();
+//     }});
 
 export default app;
